204923f397755640c586c4cb8421c50dbc9e3c6e
Denis Sokolov <denis@sokolov.cc>
2019-04-22 12:00:00 +0300

    network/dummy: allow changing the behavior when dummy data is missing

--- src/network/dummy.ts
+++ src/network/dummy.ts
@@ -1,20 +1,30 @@
 import { NetworkAdapter } from "./types";
 
+type OnMissing<Domain> = <K extends keyof Domain>(
+  kind: K,
+  id: string
+) => Domain[K];
+const defaultOnMissing: OnMissing<any> = (kind: any, id: string) => {
+  throw new Error(`Missing dummy data ${kind} ${id}`);
+};
+
 export function makeDummyAdapter<Domain>(
-  data: { [K in keyof Domain]: { [id: string]: Domain[K] } }
+  data: { [K in keyof Domain]: { [id: string]: Domain[K] } },
+  opts: {
+    onMissing?: OnMissing<Domain>;
+  } = {}
 ): NetworkAdapter<Domain> {
+  const onMissing = opts.onMissing || defaultOnMissing;
   return function({ onChange, onConnectivityChange, onPushResult }) {
     setTimeout(() => onConnectivityChange("online"), 1000);
     return {
       getAndObserve: function(kind, id) {
-        if (!data[kind][id])
-          throw new Error(`Missing dummy data ${kind} ${id}`);
         setTimeout(function() {
           onChange({
             kind,
             id,
             revision: "dummy",
-            value: data[kind][id]
+            value: id in data ? data[kind][id] : onMissing(kind, id)
           });
         }, 1500);
         return () => {};
