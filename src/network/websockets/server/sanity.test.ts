b263a3e32eb7d95f2892db23e75e662e4634071a
Denis Sokolov <denis@sokolov.cc>
2019-07-05 12:00:00 +0300

    network/websockets: add tests for reads and writes

--- src/network/websockets/server/sanity.test.ts
+++ src/network/websockets/server/sanity.test.ts
@@ -14,3 +14,66 @@ test.cb("websocket server responds to ping", t => {
   });
   client.send({ action: "ping" });
 });
+
+test.cb("websocket server retrieves data", t => {
+  const ws = makeWsMock();
+  runWebsocketServer({
+    onChangeData: async () => "success",
+    onRequestData: ({ id, kind, send }) => {
+      t.is(id, "1");
+      t.is(kind, "tasks");
+      send({ revision: "1", value: "Buy milk" });
+    },
+    _ws: ws.server
+  });
+  const client = ws.client(msg => {
+    if (msg.action === "update") {
+      t.is(msg.kind, "tasks");
+      t.is(msg.id, "1");
+      t.is(msg.revision, "1");
+      t.is(msg.value, JSON.stringify("Buy milk"));
+      t.end();
+    }
+  });
+  client.send({ action: "subscribe", kind: "tasks", id: "1" });
+});
+
+test.cb("websocket server writes data", t => {
+  const ws = makeWsMock();
+  runWebsocketServer({
+    onChangeData: async ({ id, kind, send, value }) => {
+      t.is(id, "1");
+      t.is(kind, "tasks");
+      send({ revision: "2", value });
+      return "success";
+    },
+    onRequestData: ({ send }) => {
+      send({ revision: "1", value: "Buy milk" });
+    },
+    _ws: ws.server
+  });
+  let seenUpdate = false;
+  const client = ws.client(msg => {
+    if (msg.action === "update" && msg.revision === "2") {
+      seenUpdate = true;
+      t.is(msg.kind, "tasks");
+      t.is(msg.id, "1");
+      t.is(msg.value, JSON.stringify("Buy cocoa"));
+    }
+    if (msg.action === "pushResult") {
+      t.true(seenUpdate);
+      t.is(msg.pushId, "p1");
+      t.is(msg.result, "success");
+      t.end();
+    }
+  });
+  client.send({ action: "subscribe", kind: "tasks", id: "1" });
+  client.send({
+    action: "push",
+    kind: "tasks",
+    id: "1",
+    lastSeenRevision: "1",
+    value: JSON.stringify("Buy cocoa"),
+    pushId: "p1"
+  });
+});
