e2503493cc0a710cb5f01a84cb635cc34ef7fd6f
David Kershaw <david.kershaw@theorem.co>
2019-07-11 07:04:01 -0300

    Client Ping Support (Keep Alive)

--- src/network/websockets/index.ts
+++ src/network/websockets/index.ts
@@ -16,6 +16,22 @@ export function makeWsProtocolAdapter(
   const { url } = params;
   const serialization = params.serialization || jsonSerialization;
   const socket = new ReconnectingWebSocket(url);
+
+  let pingTimer: NodeJS.Timer;
+  let timeoutTimer: NodeJS.Timer;
+
+  function restartPingMachine() {
+    clearTimeout(pingTimer);
+    clearTimeout(timeoutTimer);
+    pingTimer = setTimeout(function() {
+      send({ action: "ping" });
+      timeoutTimer = setTimeout(function() {
+        socket.reconnect();
+      }, 15000);
+    }, 15000);
+  }
+  restartPingMachine();
+
   const send = (input: {}) => {
     socket.send(JSON.stringify(input));
   };
@@ -38,6 +54,7 @@ export function makeWsProtocolAdapter(
 
       const handlers: { [action: string]: (msg: any) => void } = {
         ping: () => {},
+        pong: () => {},
         pushResult: (msg: any) => onPushResult(msg.pushId, msg.result),
         update: (msg: any) =>
           onChange({
@@ -48,6 +65,7 @@ export function makeWsProtocolAdapter(
           })
       };
       socket.onmessage = function(event) {
+        restartPingMachine();
         const msg = JSON.parse(event.data);
         const action = msg.action;
         if (!action) return console.warn(`Unrecognized message`, msg);
