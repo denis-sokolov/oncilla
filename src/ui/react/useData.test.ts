70143f0093c32a167019a27a55bd2761a79d4810
Denis Sokolov <denis@sokolov.cc>
2019-11-13 12:00:00 +0200

    react: useData update test

--- src/ui/react/useData.test.ts
+++ src/ui/react/useData.test.ts
@@ -9,3 +9,14 @@ test("useData sanity test", t => {
   t.is(task, "Buy milk");
   t.is(typeof updateTask, "function");
 });
+
+test("useData update test", async t => {
+  const { useData } = build(t, {
+    task: { "1": { revision: "1", value: "Buy milk" } }
+  });
+  const [task, updateTask] = useData("task", "1");
+  t.is(task, "Buy milk");
+  updateTask(() => "Buy charcoal");
+  const [taskAfter] = useData("task", "1");
+  t.is(taskAfter, "Buy charcoal");
+});
