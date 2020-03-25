d29e86e21a38deabd234df567a0003537c20e8c1
Denis <denis@sokolov.cc>
2020-03-25 12:00:00 +0200

    format: all files in root directory

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,12 +2,12 @@ module.exports = {
   env: {
     browser: true,
     es6: true,
-    node: true
+    node: true,
   },
   extends: ["plugin:@theorem/opinionated"],
   plugins: ["@theorem"],
   rules: {
     "@typescript-eslint/prefer-optional-chain": "off",
-    "react-hooks/exhaustive-deps": "off"
-  }
+    "react-hooks/exhaustive-deps": "off",
+  },
 };
