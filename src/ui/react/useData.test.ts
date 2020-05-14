6b3b34eafef52d95072356e73496e55011215b54
Denis Sokolov <denis@sokolov.cc>
2020-05-14 12:00:00 +0300

    react/useData: sample negative type tests

--- src/ui/react/useData.test.ts
+++ src/ui/react/useData.test.ts
@@ -1,6 +1,8 @@
 import test from "ava";
 import { build } from "./mocks";
 
+function typeTests(_f: () => void) {}
+
 test("useData sanity test", (t) => {
   const { useData } = build(t, {
     task: { "1": { revision: "1", value: "Buy milk" } },
@@ -8,6 +10,11 @@ test("useData sanity test", (t) => {
   const [task, updateTask] = useData("task", "1");
   t.is(task, "Buy milk");
   t.is(typeof updateTask, "function");
+
+  typeTests(function () {
+    // @ts-expect-error
+    useData("foo", "1");
+  });
 });
 
 test("useData update test", async (t) => {
@@ -19,6 +26,13 @@ test("useData update test", async (t) => {
   updateTask(() => "Buy charcoal");
   const [taskAfter] = useData("task", "1");
   t.is(taskAfter, "Buy charcoal");
+
+  typeTests(function () {
+    // @ts-expect-error
+    updateTask(() => 2);
+    // @ts-expect-error
+    updateTask((prev: number) => 0 as any);
+  });
 });
 
 test("useData patch", async (t) => {
@@ -30,4 +44,9 @@ test("useData patch", async (t) => {
   const [taskAfter] = useData("task", "1");
   if (taskAfter === "loading") throw new Error();
   t.is(taskAfter.title, "Buy charcoal");
+
+  typeTests(function () {
+    // @ts-expect-error
+    updateTask("title", 2);
+  });
 });
